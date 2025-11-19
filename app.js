9a5564c2f5a01504034ddbd4a3b1080cfcca401b
Rajesh Nagar <109516114+RajeshNagar701@users.noreply.github.com>
2025-11-19 16:47:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello from node_hello_student!\n => Now again updated live");
+  res.end("Hello from node_hello_student!\n Now again updated live");
 });
 
 server.listen(PORT, () => {
